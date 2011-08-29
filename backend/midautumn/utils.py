726c5aa2c070ac7d55f6093cbad518785eda7fd6
Ron Huang <ronhuang@gmail.com>
2011-08-29 14:52:19 +0800

    Further improve pretty_time().

--- backend/midautumn/utils.py
+++ backend/midautumn/utils.py
@@ -6,11 +6,49 @@
 
 
 import time
-from datetime import timedelta
+from datetime import timedelta, tzinfo
 
 
-def pretty_time(utctime, tz=None):
-    localtime = utctime + timedelta(hours=8)
+ZERO = timedelta(0)
+
+class UTC(tzinfo):
+    """UTC"""
+
+    def utcoffset(self, dt):
+        return ZERO
+
+    def dst(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+utc = UTC()
+
+class TPE(tzinfo):
+    """Time zone for Taipei"""
+
+    def utcoffset(self, dt):
+        return timedelta(hours=8)
+
+    def dst(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "Asia/Taipei"
+
+tpe = TPE()
+
+def pretty_time(dt, tz=tpe):
+    utctime = None
+    if not dt.tzname():
+        # dt is naive, assume it's UTC
+        utctime = dt.replace(tzinfo=utc)
+    else:
+        utctime = dt.astimezone(utc)
+
+    localtime = utctime.astimezone(tz)
+
     fmt = None
     if localtime.hour < 12:
         fmt = "%Y年%m月%d號 上午%I:%M:%S"
