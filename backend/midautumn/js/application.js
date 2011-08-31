1e94c3d92d825f0edd6c5ec0084c7e020f4ddb8d
Ron Huang <ronhuang@gmail.com>
2011-08-31 15:20:49 +0800

    Kick ass.

--- backend/midautumn/js/application.js
+++ backend/midautumn/js/application.js
@@ -494,4 +494,20 @@ $(document).ready(function(){
     });
   });
 
+  // kick ass
+  var kkeys = [];
+  $(window).bind('keydown', function (e) {
+    var konami = "38,38,40,40,37,39,37,39,66,65";
+
+    kkeys.push(e.keyCode);
+    if (kkeys.toString().indexOf(konami) >= 0) {
+      $(window).unbind('keydown');
+
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      document.body.appendChild(s);
+      s.src = 'http://erkie.github.com/asteroids.min.js';
+    }
+  });
+
 });
